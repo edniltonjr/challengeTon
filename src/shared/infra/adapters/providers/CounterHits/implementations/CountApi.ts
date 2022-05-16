ec00150bd602b435256cfd6e69c903ac4f167fbb
edniltonjr <jrjunior_cg@live.com>
2022-05-16 11:01:12 -0300

    refactor: Change variables apiHits, urlTon to constructor

--- src/shared/infra/adapters/providers/CounterHits/implementations/CountApi.ts
+++ src/shared/infra/adapters/providers/CounterHits/implementations/CountApi.ts
@@ -3,18 +3,22 @@ import axios from "axios";
 import { ICountsHitsProvider, IHits } from "../ICounterHits";
 
 class CountApiProvider implements ICountsHitsProvider {
+  private apiHits;
+  private urlTon;
+
+  constructor() {
+    this.apiHits = process.env.API_HITS;
+    this.urlTon = process.env.URL_TON;
+  }
+
   async countHits(): Promise<IHits> {
-    const apiHits = process.env.API_HITS;
-    const urlTon = process.env.URL_TON;
-    const { data } = await axios.get(`${apiHits}/get/${urlTon}`);
+    const { data } = await axios.get(`${this.apiHits}/get/${this.urlTon}`);
     return data;
   }
   async addHit(): Promise<IHits> {
-    const apiHits = process.env.API_HITS;
-    const urlTon = process.env.URL_TON;
-
-    const { data } = await axios.get(`${apiHits}/update/${urlTon}/?amount=1`);
-
+    const { data } = await axios.get(
+      `${this.apiHits}/update/${this.urlTon}/?amount=1`
+    );
     return data;
   }
 }
